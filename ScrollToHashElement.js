d0aefc550e966b9792030e7912c50eb2e86f6635
Akshay Sharma <sharmakshay@icloud.com>
2024-02-17 18:30:28 -0500

    using useLayoutEffect to avoid running scroll to hash before page render, and support scroll to hash if hash is in different route

--- ScrollToHashElement.js
+++ ScrollToHashElement.js
@@ -1,33 +1,28 @@
-import { useMemo, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLayoutEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ScrollToHashElement = () => {
-  let location = useLocation();
+  const location = useLocation();
 
-  let hashElement = useMemo(() => {
-    let hash = location.hash;
+  useLayoutEffect(() => {
+    const { hash } = location;
+
     const removeHashCharacter = (str) => {
       const result = str.slice(1);
       return result;
     };
 
     if (hash) {
-      let element = document.getElementById(removeHashCharacter(hash));
-      return element;
-    } else {
-      return null;
+      const element = document.getElementById(removeHashCharacter(hash));
+
+      if (element) {
+        element.scrollIntoView({
+          behavior: 'smooth',
+          inline: 'nearest',
+        });
+      }
     }
   }, [location]);
-
-  useEffect(() => {
-    if (hashElement) {
-      hashElement.scrollIntoView({
-        behavior: "smooth",
-        // block: "end",
-        inline: "nearest",
-      });
-    }
-  }, [hashElement]);
 
   return null;
 };
